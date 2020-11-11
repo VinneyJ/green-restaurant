4d13fdce3885137e16ce2f8daf939f9f9197c39b
Vince <vincentjayden49@gmail.com>
2020-11-11 20:12:27 +0300

    tracking payment

--- pizza/views.py
+++ pizza/views.py
@@ -6,7 +6,13 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.views.generic import ListView, DetailView, View
 from .forms import CheckOutForm
 from django.utils import timezone
-from .models import Item, OrderItem, Order, BillingAddress
+from .models import Item, OrderItem, Order, BillingAddress, Payment
+
+import stripe
+stripe.api_key = "sk_test_cqQW4am9ma3f3Dcy9CeRc9NS00RfhAqdoM"
+
+# `source` is obtained with Stripe.js; see https://stripe.com/docs/payments/accept-a-payment-charges#web-create-token
+
 
 # Create your views here.
 
@@ -113,8 +119,76 @@ class CheckoutView(View):
 
 class PaymentView(View):
     def get(self, *args, **kwargs):
+        # order
         return render(self.request, 'payment.html')
+    
+    def post(self, *args, **kwargs):
+        
+        order = Order.objects.get(user=self.request.user, ordered=False)
+        token = self.request.POST.get('stripeToken')
+        amount = int(order.get_total_items_price() * 100)
+        
+        try:
+            charge = stripe.Charge.create(
+                amount= amount,
+                currency="usd",
+                source= token,
+            )
+            #Create payment
+            payment = Payment()
+            payment.stripe_charge_id = charge['id']
+            payment.user = self.request.user
+            payment.amount = order.get_total_items_price()
+            payment.save()
 
+            order.ordered = True
+            order.payment = payment
+            order.save()
+
+            message.success(self.request, "Your order was successful!")
+            return redirect('/')
+        except stripe.error.CardError as e:
+            # Since it's a decline, stripe.error.CardError will be caught
+            body = e.json_body
+            err = body.get('error', {})
+
+            messages.error(self.request, f"{(err.get('message'))}")
+            return redirect('/')
+        
+        except stripe.error.RateLimitError as e:
+        # Too many requests made to the API too quickly
+            messages.error(self.request, "Rate Limit Error")
+            return redirect('/')
+    
+        except stripe.error.InvalidRequestError as e:
+        # Invalid parameters were supplied to Stripe's API
+            messages.error(self.request, "Invalid Parameters Error")
+            return redirect('/')
+    
+        except stripe.error.AuthenticationError as e:
+        # Authentication with Stripe's API failed
+        # (maybe you changed API keys recently)
+            messages.error(self.request, "Authentication Error")
+            return redirect('/')
+    
+        except stripe.error.APIConnectionError as e:
+        # Network communication with Stripe failed
+            messages.error(self.request, "Network Error")
+            return redirect('/')
+    
+        except stripe.error.StripeError as e:
+        # Display a very generic error to the user, and maybe send
+        # yourself an email
+            messages.error(self.request, "Something went wrong. You were not charged. Please try again.")
+            return redirect('/')
+    
+        except Exception as e:
+        # Something else happened, completely unrelated to Stripe
+        # Send an email to myself
+            messages.error(self.request, "A serious error. We have been notified.")
+            return redirect('/')
+        
+        
 
 
 def contact_page(request):
